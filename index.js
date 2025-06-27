110e5073b7810119bf3b46633c5722adcf082b47
Nate23-Q <nathan.isaac@student.moringaschool.com>
2025-06-27 20:01:22 +0300

    add changes

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email
+  };
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      const idElement = document.createElement("div");
+      idElement.textContent = object.id;
+      document.body.appendChild(idElement);
+    })
+    .catch(function(error) {
+      const errorElement = document.createElement("div");
+      errorElement.textContent = error.message;
+      document.body.appendChild(errorElement);
+    });
+}
